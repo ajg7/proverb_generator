221de92562c2f7c59f508490fe41b1e4075ec827
ajg7 <ajgebara@gmail.com>
2020-09-17 11:36:09 -0500

    added axioswithauth

--- src/axiosWithAuth.js
+++ src/axiosWithAuth.js
@@ -1,1 +1,12 @@
 import axios from "axios";
+
+export const axiosWithAuth = () => {
+    const token = "43bf74bd360ac5fd6885bd8574aa5c1c37a57f51";
+
+    return axios.create({
+        headers: {
+            Authorization: token 
+        },
+        baseURL: "api.esv.org"
+    })
+}
