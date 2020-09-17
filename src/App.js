d009ec7576a0aa2f3c9b229ee565cd0f12fcf65a
ajg7 <ajgebara@gmail.com>
2020-09-17 12:53:25 -0500

    able to render random verse in Proverbs onto the screen

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Verse from "./components/Verse";
 function App() {
   return (
     <div className="App">
-      <h1>The Fear of the Lord is the Beginning of Wisdom</h1>
       <Verse />
     </div>
   );
--- src/components/Verse.js
+++ src/components/Verse.js
@@ -1,21 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../axiosWithAuth";
 
 const Verse = props => {
+    const [passage, setPassage] = useState([])
 
-        axiosWithAuth().get("v3/passage/text?q=Proverbs+2:4")
+    useEffect(() => {
+        const randomChapter = Math.round(Math.random() * (31-1));
+        const randomVerse = Math.round(Math.random() * (31-1));
+
+        axiosWithAuth().get(`v3/passage/text?q=Proverbs+${randomChapter}:${randomVerse}`)
             .then(response => {
-                console.log(response.data.passages)
+                const passage = response.data.passages;
+                setPassage(passage)
             })
             .catch(error => {
                 console.log(error)
             })
-    
-
-
+    }, [])
     return(
         <div className="verse-card">
-        
+            <h2>{passage}</h2>
         </div>
     )
 }
