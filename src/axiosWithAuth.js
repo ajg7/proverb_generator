7629670642c2faf2069407db67e79184de63a305
ajg7 <ajgebara@gmail.com>
2020-09-17 13:30:22 -0500

    added a button and click handler that allows for random verses everytime you click

--- src/axiosWithAuth.js
+++ src/axiosWithAuth.js
@@ -4,6 +4,13 @@ export const axiosWithAuth = () => {
     const token = "43bf74bd360ac5fd6885bd8574aa5c1c37a57f51";
 
     return axios.create({
+        params: {
+            'include-headings': false,
+            'include-footnotes': false,
+            'include-verse-numbers': false,
+            'include-short-copyright': false,
+            'include-passage-references': false
+        },
         headers: {
             Authorization: token 
         },
--- src/components/Verse.js
+++ src/components/Verse.js
@@ -2,24 +2,41 @@ import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../axiosWithAuth";
 
 const Verse = props => {
-    const [passage, setPassage] = useState([])
+    const [passage, setPassage] = useState([]);
+    const [verse, setVerse] = useState([]);
+    const [translation, setTranslation] = useState("");
 
-    useEffect(() => {
+
+    const clickHandler = () => {
         const randomChapter = Math.round(Math.random() * (31-1));
         const randomVerse = Math.round(Math.random() * (31-1));
 
         axiosWithAuth().get(`v3/passage/text?q=Proverbs+${randomChapter}:${randomVerse}`)
             .then(response => {
+                console.log(response.data)
                 const passage = response.data.passages;
-                setPassage(passage)
+                setPassage(passage);
+                setVerse(response.data.query);
+                setTranslation("ESV");
             })
             .catch(error => {
                 console.log(error)
             })
+    }
+
+    useEffect(() => {
+        clickHandler()
     }, [])
+    
     return(
-        <div className="verse-card">
-            <h2>{passage}</h2>
+        <div className="generator">
+            <div className="verse-card">
+                <h2>{passage}</h2>
+                <h2>{verse} {translation}</h2>
+            </div>
+            <div className="random-verse-button">
+                <button onClick={clickHandler}>Random Verse!</button>
+            </div>
         </div>
     )
 }
